077590a470e5880b5491332cb8082a8c202a8c6a
mohitjain610 <mj353534@gmail.com>
2026-01-10 21:22:01 +0530

    4st program

---
+++ basic program/Area_of_a_circle .java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Area_of_a_circle {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int radius= sc.nextInt();
+        float Area = 3.14f * radius * radius;
+        System.out.println(Area);
+    }
+}
