f94c17b523f8709b7f0d1959da9b2bdd87b83944
mohitjain610 <mj353534@gmail.com>
2026-01-19 21:28:15 +0530

    NaturalSumWhile

---
+++ basic program/NaturalSumWhile.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class NaturalSumWhile {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        if (n >= 1) {
+            int formulaSum = n * (n + 1) / 2;
+            int loopSum = 0;
+            int i = 1;
+
+            while (i <= n) {
+                loopSum += i;
+                i++;
+            }
+
+            System.out.println("Formula Sum = " + formulaSum);
+            System.out.println("Loop Sum = " + loopSum);
+        } else {
+            System.out.println("Not a natural number");
+        }
+    }
+}
