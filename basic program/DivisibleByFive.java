e7b7eeae8ff5782f0f6a72b5298d0af40c41a113
mohitjain610 <mj353534@gmail.com>
2026-01-19 16:28:29 +0530

    DivisibleByFive

---
+++ basic program/DivisibleByFive.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DivisibleByFive {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int number = input.nextInt();
+
+        if (number % 5 == 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+  
+    }
+}
