154aefd5dea5fe19b3948387a0bfdc7cf44e2fec
mohitjain610 <mj353534@gmail.com>
2026-01-19 22:03:47 +0530

    DigitCounter

---
+++ basic program/DigitCounter.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class DigitCounter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        int count = 0;
+        while (number != 0) {
+            number = number / 10;
+            count++;
+        }
+        System.out.println("Digits: " + count);
+    }
+}
