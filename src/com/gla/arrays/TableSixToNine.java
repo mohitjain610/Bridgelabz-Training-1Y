b6f978b1768f2e9c1020c3d5157718515e371134
mohitjain610 <mj353534@gmail.com>
2026-02-02 22:28:44 +0530

    [Mohit jain]TableSixToNine

---
+++ src/com/gla/arrays/TableSixToNine.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class TableSixToNine {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        int[] results = new int[4];
+        int index = 0;
+        for (int i = 6; i <= 9; i++) {
+            results[index] = number * i;
+            index++;
+        }
+        index = 0;
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + results[index]);
+            index++;
+        }
+    }
+}
