6baf58ac2fa6c2279149f7792f4dfd8787d7ebf8
mohitjain610 <mj353534@gmail.com>
2026-02-18 21:01:44 +0530

    [Mohit Jain]Add Animal

---
+++ src/com/gla/inheritance/Main.java
@@ -0,0 +1,44 @@
+package com.gla.inheritance;
+
+class Animal {
+    String name;
+    int age;
+
+    void makeSound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Cat meows");
+    }
+}
+
+class Bird extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Bird chirps");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal a1 = new Dog();
+        Animal a2 = new Cat();
+        Animal a3 = new Bird();
+
+        a1.makeSound();
+        a2.makeSound();
+        a3.makeSound();
+    }
+}
+
