580e3a53bc6fe1c7114343917960efa49b108a6a
mohitjain610 <mj353534@gmail.com>
2026-03-25 08:22:05 +0530

    [Mohit Jain]Add Q3

---
+++ src/Q3.java
@@ -0,0 +1,22 @@
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String msg) {
+        super(msg);
+    }
+}
+
+public class Q3 {
+    static void validateAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or above");
+        }
+        System.out.println("Access granted!");
+    }
+
+    public static void main(String[] args) {
+        try {
+            validateAge(16);
+        } catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
